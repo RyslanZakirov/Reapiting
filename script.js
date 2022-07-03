e47b354c5d4808b0cd7cffbcb6c7adf8d8419ea8
Ruslan <rus.zakirov.2000@bk.ru>
2022-07-03 17:15:24 +0700

    Add function: parseArr

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ document.addEventListener("DOMContentLoaded", e => {
 
     console.log("Script is working.");
 
+    const parseArr = (arr) => {
+        return arr.map(elem => {
+            return elem * (Math.random() * 100)
+        })
+    }
+
 })
